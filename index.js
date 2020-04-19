f9e9de3734b559709291e25846a3dea71b3bea97
Joel <joelkaplan1@gmail.com>
2020-04-18 22:11:48 -0700

    Added back server for local testing

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000
+
+app.use(express.static(path.join(__dirname)));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
